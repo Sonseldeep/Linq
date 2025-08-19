ea8b1cd93feca66c1dd42c041d7f76a8aa22b2de
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-08-20 02:02:52 +0545

    add get all ang get by id in category

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,6 +1,35 @@
+using Ecommerce.API.Data;
+using Ecommerce.API.Mapping;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace Ecommerce.API.Controllers;
 
-public class CategoriesController
+[ApiController]
+public class CategoriesController : ControllerBase
 {
+    private readonly EcommerceDbContext _dbContext;
+
+    public CategoriesController(EcommerceDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet("api/categories")]
+    public async Task<IActionResult> GetAll()
+    {
+        var categories = await _dbContext   .Categories.AsNoTracking().Select(category => category.ToResponseDto()).ToListAsync();
+        return Ok(categories);
+    }
     
+    [HttpGet("api/categories/{id:guid}")]
+    public async Task<IActionResult> Get([FromRoute] Guid id)
+    {
+        var category = await _dbContext.Categories.AsNoTracking().SingleOrDefaultAsync(c => c.Id == id);
+        if (category is  null)
+        {
+            return NotFound();
+        }
+        return Ok(category.ToResponseDto());
+    }
 }
