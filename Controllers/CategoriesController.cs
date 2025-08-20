7238af100218aa53d4a6463ae079ff6e1c40e799
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-08-20 12:20:27 +0545

    add CRUD operations for categories in CategoriesController

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,7 +1,10 @@
 using Ecommerce.API.Data;
+using Ecommerce.API.DTOs.Category;
 using Ecommerce.API.Mapping;
+using Ecommerce.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Ecommerce.API.Controllers;
 
@@ -31,5 +34,56 @@ public class CategoriesController : ControllerBase
             return NotFound();
         }
         return Ok(category.ToResponseDto());
+    }
+
+    [HttpPost("api/categories")]
+    public async Task<IActionResult> Create([FromBody] CreateCategoryDto dto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var category = new Category
+        {
+            Name = dto.Name
+        };
+        await _dbContext.Categories.AddAsync(category);
+        await _dbContext.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(Get), new { id = category.Id }, category.ToResponseDto());
+    }
+
+    [HttpPut("api/categories/{id:guid}")]
+    public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateCategoryDto dto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var category = await _dbContext.Categories.SingleOrDefaultAsync(x => x.Id == id);
+        if( category is null)
+        {
+            return NotFound();
+        }
+        category.Name = dto.Name;
+        await _dbContext.SaveChangesAsync();
+
+        return NoContent();
+    }
+    
+    [HttpDelete("api/categories/{id:guid}")]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+        var category = await _dbContext.Categories.SingleOrDefaultAsync(x => x.Id == id);
+        if( category is null)
+        {
+            return NotFound();
+        }
+        _dbContext.Categories.Remove(category);
+        await _dbContext.SaveChangesAsync();
+
+        return NoContent();
     }
 }
